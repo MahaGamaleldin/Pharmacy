6b3727fe6a1124d032792014077e3707d09bcbd1
Maha Gamaleldeen <maha.gamaleldeen@yahoo.com>
2022-09-06 02:26:53 +0200

    Initial Commit

---
+++ Pharmacy/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Pharmacy
+//
+//  Created by Maha on 9/6/22.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Pharmacy/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Pharmacy
+//
+//  Created by Maha on 9/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
